f5f482c82fa9b3e6f22706471be6a539d05d485c
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-01-26 16:35:57 +0400

    2

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+//! 1
+// function fnc1(...numbers) {
+//   let sum = 0;
+//   for (let x of numbers) {
+//     sum += x;
+//   }
+//   return sum;
+// }
+// let result = fnc1(10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(result);
+
+// //!
+
+// let fnc2 = (...numbers1) => {
+//   let sum1 = 0;
+//   for (let x1 of numbers1) {
+//     sum1 += x1 ;
+//   }
+//   return sum1 ;
+// }
+
+// let result1 = fnc2 (10, 50, 6, 7, 8, 11, 6, 3, 9);
+// console.log(result1);
+
+//! 2
+
+// let user1 = {
+//   fisrtName: "giorgi",
+//   lastName: "saakadze",
+//   age: 32,
+//   isloggedin: true,
+// };
+
+//  let result = user1.fisrtName + " " + user1.lastName ;
+//  console.log(result);
+//  let result1 = `${user1.fisrtName} ${user1.lastName}`
+//  console.log(result1);
+
+// let fnc3 = function (x,y){
+//     if (user1.isloggedin ===x){
+//         return `${user1.fisrtName}  ${user1.lastName}`
+//     }
+//     else if (user1.isloggedin === y){
+//         return "false";
+//     }
+// }
+// let result = fnc3(true , false);
+// console.log(result);
+
+//!3 
